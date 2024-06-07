deca49e34c0a99ca7538271fdd78a7ac42d65f16
K Gopal Krishna <mail@kayg.org>
2024-06-07 12:52:06 +0530

    Quartz sync: Jun 7, 2024, 12:52 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -16,10 +16,8 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.MobileOnly(Component.Explorer({ title: "Finder" })),
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
-    Component.ContentMeta({ showComma: false }),
     Component.TagList(),
     Component.MobileOnly(Component.TableOfContents()),
   ],
